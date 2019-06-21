5659d3a209d09b10c1a87e03276134d1b2c686ec
pluto-atom-4 <genko2@hotmail.com>
2019-06-21 16:28:18 -0700

    Updated the test to validate the trim and split

--- src/test/java/internal/example/language/StringTests.java
+++ src/test/java/internal/example/language/StringTests.java
@@ -73,7 +73,7 @@ public class StringTests {
   @Test
   public void returnTrimmedWhiteSpaces() {
     String str = "    -12345 abcd";
-    String expect = "-12345 ";
+    String expect = "-12345";
     System.out.println(str.trim());
     assertThat(str.trim().split(" ")).hasSize(2);
     assertThat(str.trim().split(" ")[0]).isEqualTo(expect);
