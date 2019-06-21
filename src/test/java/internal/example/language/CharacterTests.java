bcbeb9a8e36219fb80b6b1d339b6dda791943bad
pluto-atom-4 <genko2@hotmail.com>
2019-06-20 18:08:06 -0700

    Updated the test to validate the zero-based index

--- src/test/java/internal/example/language/CharacterTests.java
+++ src/test/java/internal/example/language/CharacterTests.java
@@ -2,15 +2,23 @@ package internal.example.language;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CharacterTests {
 
   @Test
-  public void shouldReturnOneForA() {
+  public void shouldReturnZeroBasedIndexForAlphabets() {
     char a = 'A';
     char b = 'B';
     char c = 'C';
-    System.out.println(a - 64);
-    System.out.println(b - 64);
-    System.out.println(c - 64 );
+    assertThat(a - 'A').isEqualTo(0);
+    assertThat(b - 'A').isEqualTo(1);
+    assertThat(c - 'A').isEqualTo(2);
+  }
+
+  @Test
+  public void shouldReturnString() {
+    char c = 'a';
+    assertThat(String.valueOf(c)).isEqualTo("a");
   }
 }
