e1227ef0a39f73b3e423fbb5bfa94a9eaaa3f94b
pluto-atom-4 <genko2@hotmail.com>
2019-06-23 10:43:42 -0700

    Added the test to validate LCD GCM with stream API

---
+++ src/test/java/internal/example/language/StreamTests.java
@@ -0,0 +1,26 @@
+package internal.example.language;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StreamTests {
+
+  @Test
+  public void returnLcm() {
+    var numbers = List.of(9, 54);
+    assertThat(numbers.stream().reduce(0, (x, y) -> gcd(x, y))).isEqualTo(9);
+  }
+
+  @Test
+  public void returnGcd() {
+    var numbers = List.of(9, 54);
+    assertThat(numbers.stream().reduce(1, (x, y) -> x * (y / gcd(x, y)))).isEqualTo(9);
+  }
+
+  private int gcd(int x, int y) {
+    return (y == 0) ? x : gcd(y, x%y);
+  }
+}
