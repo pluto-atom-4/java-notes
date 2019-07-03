2b4ce900288287a97187076a7f5859866b4016c3
pluto-atom-4 <genko2@hotmail.com>
2019-07-03 10:47:55 -0700

    Added the test of Dequeue operation

---
+++ src/test/java/internal/example/language/QueueTests.java
@@ -0,0 +1,23 @@
+package internal.example.language;
+
+import org.junit.Test;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class QueueTests {
+
+  @Test
+  public void shouldReturnSize() {
+    Deque<Integer> que = new LinkedList<>();
+
+    que.addFirst(10);
+    que.add(5);
+    que.addLast(1);
+
+    assertThat(que.removeLast()).isNotEqualTo(10).isEqualTo(1);
+    assertThat(que.remove()).isNotEqualTo(5).isEqualTo(10);
+  }
+}
